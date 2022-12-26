094e4c2a13d619c204bf9dc3e3a7960885365b7c
Joshu <joshua.munoz@i3television.es>
2022-12-26 13:53:18 +0100

    asdf

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.post("/convert", upload.single("file"), (req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // res.setHeader('Access-Control-Allow-Credentials', true);
   ffmpeg(file.path)
     .outputOptions([
       "-vf",
